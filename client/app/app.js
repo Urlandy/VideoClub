33320fda7f7f4636d54e1d0169a868ae09978e98
Urlandy Ibarra P <Urlandy Ibarra P>
2017-08-12 13:11:28 -0500

    Primer Commit

---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('videoClubApp', [
+	'videoClubApp.constants',
+	 'ngCookies',
+	 'ngResource',
+	 'ngSanitize',
+     'ui.router',
+     'ui.bootstrap'
+  ])
+.constant("API","http://localhost:25956/adsi2017_backend_completo")
+
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
